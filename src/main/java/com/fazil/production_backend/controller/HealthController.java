2370ca62720fc0760428831d75d0974d9a826bef
MohamedFazil1406 <mohamedfazil01406@gmail.com>
2026-08-08 21:16:07 +0530

    second commit

---
+++ src/main/java/com/fazil/production_backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.fazil.production_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Production backend is running!";
+    }
+}
---
+++ src/test/java/com/fazil/production_backend/ProductionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazil.production_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
